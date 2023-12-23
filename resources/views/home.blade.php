a185bb69669da1e18b3d80fc85fb26297996c4a8
Abdullah Asim <abdullah.asim.0807@gmail.com>
2023-12-23 15:50:00 +0500

    products grid

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,22 +44,22 @@
     </nav>
 
     <!-- Page Content -->
-    <div class="container">
-        <div class="row">
-        <div class="container">
-        <h1>All Ads</h1>
-
-        @foreach ($products as $product)
-            <div>
-                <h2>{{ $product->title }}</h2>
-                <p>{{ $product->description }}</p>
-                <p>Price: ${{ $product->price }}</p>
+            <div class="container">
+                <h1>All Ads</h1>
+                <div class="row row-cols-1 row-cols-md-2">
+                @foreach ($products as $product)
+                    <div class="card col-xs-4" style="width: 18rem; margin: 0.5rem;">
+                        <img class="card-img-top" src="assets/image_def.jpg" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">{{ $product->title }}</h5>
+                          <p class="card-text">{{ $product->description }}</p>
+                          <p class="card-text">Price: <span style="color: #2a5ac0; font-weight: 700;">${{ $product->price }}</span></p>
+                          <a href="product.html" class="btn btn-primary">CHECKOUT!</a>
+                        </div>
+                    </div>
+                @endforeach
+                </div>
             </div>
-            <hr>
-        @endforeach
-    </div>
-        </div>
-    </div>
 
     <!-- Footer -->
     <footer class="bg-dark text-light py-3">
