28cacf1e7df78be1e8082f6fe9f2f29b07ddaaac
Abdullah Asim <abdullah.asim.0807@gmail.com>
2023-12-24 14:15:07 +0500

    login page redesign

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,25 +3,19 @@
 @section('title', 'Login')
 @section('content')
 
-<div class="column">
-    <div class="col-md-6">
-        <div class="loginDiv">
-            <div class="jumbotron">
-                <h1 class="display-4">Login</h1>
-            </div>
-            <form method="POST" action="/login">
-            @csrf
-                <fieldset title="Basic inputs" id="ïnput">
-                    <label for="text-input">Email:</label>
-                    <input type="text" id="username" name="text-input" placeholder="Enter text here">
-                    <label for="password-input">Password:</label>
-                    <input type="password" id="password" name="password-input" placeholder="Enter your password">
-                    <input type="submit" value="Login" onclick="return validateForm();">
-                </fieldset>
-            </form>
-            <div><h3 id="error"></h3></div>
-        </div>
-    </div>
+<div class="loginDiv">
+    <h1>Login</h1>
+    <form method="POST" action="/login">
+    @csrf
+    <fieldset title="Login" id="input">
+            <label for="username-input">Email</label>
+            <input type="text" id="username" name="username-input">
+            <label for="password-input">Password</label>
+            <input type="password" id="password" name="password-input">
+        <input type="submit" value="Login" onclick="return validateForm();">
+    </fieldset>
+    </form>
+    <div><h3 id="error"></h3></div>
     <h3>New Here? <a href="register">Register</a></h3>
 </div>
 @endsection
